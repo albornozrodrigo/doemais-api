68e53a002fd7ed773372249cd5b3f3b7a510068d
Rodrigo Albornoz <rodrigo.albornoz@apponteweb.com>
2018-09-11 11:08:32 -0300

    fix deploy heroku

--- app/config/config.js
+++ app/config/config.js
@@ -1,5 +1,6 @@
 module.exports = {
-    dbHost: 'mongodb://localhost:27017/doemais',
+    //dbHost: 'mongodb://localhost:27017/doemais',
+    dbHost: 'mongodb://'+process.env.MONGO_LOGIN+':'+process.env.MONGO_PWD+'@ds251362.mlab.com:51362/doemais',
     port: 3003,
     secret: 'doemais',
     viewsPath: './app/web'
