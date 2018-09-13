ca2d39db33963000598da0820153a1310ec2445b
Rodrigo Albornoz <rodrigo.albornoz.f@gmail.com>
2018-09-13 12:17:41 -0300

    teste dialogflow

--- app/modules/api/bot/controller.js
+++ app/modules/api/bot/controller.js
@@ -1,28 +1,27 @@
 this.getFulfillment = async (req, res) => {
-    return res.json({data: req.body});
-    const body = req.body;
-    let number1 = body.queryResult.parameters.number1;
-    let number2 = body.queryResult.parameters.number2;
-    let action = body.queryResult.action;
-    let result = 0;
+    // const body = req.body;
+    // let number1 = body.queryResult.parameters.number1;
+    // let number2 = body.queryResult.parameters.number2;
+    // let action = body.queryResult.action;
+    // let result = 0;
 
-    switch (action) {
-        case 'somar':
-            result = number1 + number2;
-        break;
-        case 'subtrair':
-            result = number1 - number2;
-        break;
-        case 'dividir':
-            result = number1 / number2;
-        break;
-        case 'multiplicar':
-            result = number1 * number2;
-        break;
-    }
+    // switch (action) {
+    //     case 'somar':
+    //         result = number1 + number2;
+    //     break;
+    //     case 'subtrair':
+    //         result = number1 - number2;
+    //     break;
+    //     case 'dividir':
+    //         result = number1 / number2;
+    //     break;
+    //     case 'multiplicar':
+    //         result = number1 * number2;
+    //     break;
+    // }
 
     res.json({
-        fulfillmentText: result
+        fulfillmentText: req.body.queryResult.fulfillmentText + ' PORRA'
     });
 }
 
